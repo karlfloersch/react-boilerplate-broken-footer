d534c812479864ba6b5629ddedeb45641d99f341
Karl Floersch <karl@karlfloersch.com>
2016-07-23 14:21:57 -0400

    Broken floating footer

--- app/components/Footer/index.js
+++ app/components/Footer/index.js
@@ -6,12 +6,7 @@ import styles from './styles.css';
 function Footer() {
   return (
     <footer className={styles.footer}>
-      <section>
-        <p>This project is licensed under the MIT license.</p>
-      </section>
-      <section>
-        <p>Made with love by <A href="https://twitter.com/mxstbr">Max Stoiber</A>.</p>
-      </section>
+      why is it broken?
     </footer>
   );
 }
